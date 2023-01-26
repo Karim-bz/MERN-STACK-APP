3c691507ef9485848ff4cc09dd463348a4f28a35
B.Karim <41644989+Karim-bz@users.noreply.github.com>
2023-01-26 15:00:32 +0100

    add date fns package to convert createdAt Date

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -16,7 +16,8 @@ export const workoutsReducer = (state, action) => {
             return {
                 workouts: state.workouts.filter((w) => w._id !== action.payload._id)
             }
-        default: state
+        default: 
+            return state
     }
 }
 
